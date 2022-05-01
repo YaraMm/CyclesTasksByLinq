50cff63824bfac8915c3d1f0432ae518f2ba2f4a
YaraMm <for.steam2016@mail.ru>
2022-05-01 22:52:02 +0400

    первое задание мин макс линком без индексов

---
+++ мин, макс линком/мин, макс линком/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace мин__макс_линком
+{
+    internal class Program
+    {
+        static int MinByLinq(IEnumerable<int> myList) {return myList.Min();}
+        static int MaxByLinq(IEnumerable<int> myList) {return myList.Max();}
+
+        static int MinIndexByLinq(IEnumerable<int> myList) => myList.IndexOf(MinByLinq(myList));
+        static void Main(string[] args)
+        {
+        }
+    }
+}
