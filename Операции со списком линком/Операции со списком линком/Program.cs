b2b0ff8a756c31a3997fb51b6b1ffdb365dabe5a
YaraMm <for.steam2016@mail.ru>
2022-05-02 12:54:11 +0400

    второе задание "операции со списком" доделать

---
+++ Операции со списком линком/Операции со списком линком/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Операции_со_списком_линком
+{
+    internal class Program
+    {
+        public static int Summ (IEnumerable<int> myList) => myList.Sum();
+        public static int Sqr(List<int> myList)
+        {
+            int res = 1;
+            for(int i = 0; i < myList.Count; i++) { res *= myList[i]; }
+            return res;
+        }
+
+        public static int MiddleNum(IEnumerable<int> myList)
+        {
+            return Summ(myList) / myList.Length;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
